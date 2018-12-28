b9efd6ae3c29f8be7edd80e958d0fb6d42eeb410
Adamsdurga <42992052+Adamsdurga@users.noreply.github.com>
2018-12-28 14:09:32 +0530

    Create python66.py

---
+++ python66.py
@@ -0,0 +1,10 @@
+def fact(n) :
+    if n > 1:
+       for i in range(2,m):
+          if (n % i) == 0:
+            return "no"
+        return "yes"
+    return "no"
+n= int(input(
+   "Enter value"))
+print(fact(n))

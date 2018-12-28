f10a3c1d1d60d159fb62f382df6312a01904be37
Adamsdurga <42992052+Adamsdurga@users.noreply.github.com>
2018-12-28 11:53:56 +0530

    Create python60.py

---
+++ python60.py
@@ -0,0 +1,15 @@
+a=int(input("Enter Fibonacci Range"))
+a1 = 0
+a2 = 1
+count = 0
+l=[]
+if n == 1:
+    l.append(a1)
+else:
+    while count < a:
+        l.append(n1)
+        ath = a1 + a2
+        a1 = a2
+        a2 = ath
+        count += 1
+print(" ".join(str(y) for y in l))

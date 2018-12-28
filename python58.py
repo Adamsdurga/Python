36f3bd6800038dd13b1819a4a9997da500c5dec5
Adamsdurga <42992052+Adamsdurga@users.noreply.github.com>
2018-12-28 11:29:59 +0530

    Create python58.py

---
+++ python58.py
@@ -0,0 +1,10 @@
+x=int(input("enter the no"))
+y=int(input("enter the no"))
+d=[]
+for i in range(0,x):
+    z=int(input("enter"))
+    d.append(z)
+if(y in d):
+    print("yes")
+else:
+    print("no")
